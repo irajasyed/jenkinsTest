9f689294c02dd982a042d14114f79f10acafd5ba
rajasyed <rajasyedab@gmail.com>
2016-02-19 11:54:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #ssh -t -t mohitbhagwani.s@192.168.4.78
 #cp $WORKSPACE/hello.php /var/www/html/hello.php
-scp $WORKSPACE/hello.php mohitbhagwani.s@192.168.4.78:/var/www/xhtml
+scp $WORKSPACE/hello.php mohitbhagwani.s@192.168.4.78:/var/www/html
