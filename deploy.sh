d2229e453a170bd225e5abb08b9d579e006efae0
rajasyed <rajasyedab@gmail.com>
2016-02-19 11:06:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ssh -t rajasyedabuthahir.j@192.168.4.75
+ssh -t mohitbhagwani.s@192.168.4.78
 cp $WORKSPACE/hello.php /var/www/html/hello.php
 #scp $WORKSPACE/hello.php rajasyedabuthahir.j@localhost:/home/local/PAYODA/rajasyedabuthahir.j/var/www/html
